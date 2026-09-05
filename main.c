19de8e1a8e3ab8f2ff92950bcae59792e49268ab
Harsha G <harsha150904@gmail.com>
2026-09-05 15:08:52 +0000

    MP3 tag Read and Edit completed

--- main.c
+++ main.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 #include "view.h"
 #include "edit.h"
 #include "types.h"
 
 int main(int argc, char *argv[])
 {
-    if (argc < 3)
+    if (argc < 2)
     {
         printf("Usage:\n");
-        printf("./a.out -v <mp3 file>\n");
-        printf("./a.out -e <mp3 file> -t <new title>\n");
-        printf("./a.out -e <mp3 file> -a <new artist>\n");
-        printf("./a.out -e <mp3 file> -A <new album>\n");
-        printf("./a.out -e <mp3 file> -y <new year>\n");
-        printf("./a.out -e <mp3 file> -g <new genre>\n");
-        printf("./a.out -e <mp3 file> -c <new comment>\n");
+        printf("./mp3_tag_reader -v <file.mp3>\n");
+        printf("./mp3_tag_reader -e <file.mp3> <option> <value>\n");
+        printf("./mp3_tag_reader -h\n");
 
         return 1;
     }
 
-    // View operation 
+    //View operation
     if (strcmp(argv[1], "-v") == 0)
     {
         ToviewInfo view;
@@ -28,6 +24,7 @@ int main(int argc, char *argv[])
         if (argc != 3)
         {
             printf("Invalid arguments for view\n");
+            printf("Usage: ./mp3_tag_reader -v <file.mp3>\n");
             return 1;
         }
 
@@ -43,7 +40,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    //Edit operation
+    // Edit operation
     else if (strcmp(argv[1], "-e") == 0)
     {
         ToeditInfo edit;
@@ -51,6 +48,15 @@ int main(int argc, char *argv[])
         if (argc != 5)
         {
             printf("Invalid arguments for edit\n");
+            printf("Usage: ./mp3_tag_reader -e <file.mp3> <option> <value>\n");
+            printf("\nOptions:\n");
+            printf("  -t <value>    Modify Title\n");
+            printf("  -A <value>    Modify Artist\n");
+            printf("  -a <value>    Modify Album\n");
+            printf("  -y <value>    Modify Year\n");
+            printf("  -m <value>    Modify Content Type\n");
+            printf("  -c <value>    Modify Composer\n");
+
             return 1;
         }
 
@@ -66,10 +72,52 @@ int main(int argc, char *argv[])
         }
     }
 
+    //Help operation 
+    else if (strcmp(argv[1], "-h") == 0)
+    {
+        if (argc != 2)
+        {
+            printf("ERROR: Invalid arguments for help\n");
+            printf("Usage: ./mp3_tag_reader -h\n");
+
+            return 1;
+        }
+
+        printf("\n=====================================\n");
+        printf("       MP3 TAG READER AND EDITOR\n");
+        printf("=====================================\n");
+
+        printf("\nUsage:\n");
+
+        printf("\nView MP3 tags:\n");
+        printf("  ./mp3_tag_reader -v <file.mp3>\n");
+
+        printf("\nEdit MP3 tags:\n");
+        printf("  ./mp3_tag_reader -e <file.mp3> <option> <value>\n");
+
+        printf("\nEdit Options:\n");
+        printf("  -t <value>    Modify Title\n");
+        printf("  -A <value>    Modify Artist name\n");
+        printf("  -a <value>    Modify Album name\n");
+        printf("  -y <value>    Modify Year\n");
+        printf("  -m <value>    Modify Content type\n");
+        printf("  -c <value>    Modify Composer\n");
+
+        printf("\nHelp:\n");
+        printf("  ./mp3_tag_reader -h\n");
+
+        printf("\n=====================================\n");
+    }
+
+    //Invalid operation 
     else
     {
-        printf("Invalid option\n");
-        printf("Use -v for view or -e for edit\n");
+        printf("ERROR: Invalid option '%s'\n", argv[1]);
+
+        printf("\nValid options:\n");
+        printf("  ./mp3_tag_reader -v <file.mp3>\n");
+        printf("  ./mp3_tag_reader -e <file.mp3> <option> <value>\n");
+        printf("  ./mp3_tag_reader -h\n");
 
         return 1;
     }
